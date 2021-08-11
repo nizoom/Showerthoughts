8f5d28176260911ae4038fd015ada1fccccf990f
Nissim <nissimram1812@gmail.com>
2021-08-11 17:58:06 -0400

    first commit

---
+++ src/components/header/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./header.css"
+
+const Header = () => {
+    return (
+        <div className="title_div">
+
+            <h1 className="title_h1"> Showerthoughts.</h1>
+            <h2 className="subtitle"> Hear the quite moments </h2>
+
+        </div>
+    )
+}
+
+export default Header;
