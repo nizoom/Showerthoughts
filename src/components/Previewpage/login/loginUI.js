2dc494954d1f11cde92a92e1c24c3af232317963
Nissim <nissimram1812@gmail.com>
2021-08-16 16:25:27 -0400

    login menu draft

--- src/components/Previewpage/login/loginUI.js
+++ src/components/Previewpage/login/loginUI.js
@@ -5,8 +5,22 @@ import "./loginUI.css"
 const LoginUI = (props) => {
     return (
         <div className="login-box">
-            <form >
+            <form>
                 <h2> Login </h2>
+                <div className="username-field">
+                    <label htmlFor="username"> Username: </label>
+                    <input type="text" name="username" id="username" />
+                </div >
+                <div className="password-field">
+                    <label htmlFor="password"> Password: </label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <div>
+                    <button type="submit" className="submit-login"> submit </button>
+                </div>
+
+                <h3> Click [here] to create an account</h3>
+
 
             </form>
         </div>
