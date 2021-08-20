4c319b989e1e12f74456d9a19d5885b811ee195f
Nissim <nissimram1812@gmail.com>
2021-08-20 16:33:57 -0400

    sample news feed skeleton

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -3,6 +3,7 @@ import Header from "../header/header";
 import LoginBtn from "../Previewpage/login/loginbtn"
 import "./dashboard.css"
 import { useAuth } from "../../contexts/authcontext";
+import NavMenu from "../navmenu/nav"
 
 const Dashboard = (props) => {
 
@@ -14,21 +15,33 @@ const Dashboard = (props) => {
                 <Header />
                 <LoginBtn />
             </div>
-
+            <NavMenu />
             <div className="dashboard-wrapper">
 
                 <h2> <em> Email: </em> {currentUser.email}</h2>
-                <h2> Dashboard</h2>
-                <section>
-                    <h3> Posts </h3>
-                    <h3> Feed </h3>
-                    <h3> New Showerthought</h3>
+                <h2> username :{currentUser.displayName}</h2>
+                <h2> Shower-induced thoughts near you: </h2>
+
+                <section className="feed-wrapper">
+                    <ul className="feed-list">
+                        <li>
+                            <article> <u> Post 1, from Thaddeus McThot </u></article>
+                        </li>
+                        <li>
+                            <article> <u> Post 2, from Nissua Pappise </u></article>
+                        </li>
+                    </ul>
+
                 </section>
             </div>
 
 
-        </div>
+        </div >
     )
 }
 
 export default Dashboard
+
+{/* <h3> Posts </h3>
+                    <h3> Feed </h3>
+                    <h3> New Showerthought</h3> */}
